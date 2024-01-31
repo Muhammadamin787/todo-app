45c0fe39cdc530ef5f561fa135d9edaf78087bc2
= <=>
2024-01-31 15:28:09 +0500

    .

--- app/api/board/[id]/route.ts
+++ app/api/board/[id]/route.ts
@@ -1,27 +1,26 @@
 import appwriteSDK from "@/services/AppConfigServer";
 import { DB } from "@/utils/constans";
 import { NextRequest, NextResponse } from "next/server";
-import { cookies, headers } from "next/headers";
+import { headers } from "next/headers";
 import { setToken } from "@/api/DBFunctions";
-import { ID, Permission, Role } from "node-appwrite";
 
-export async function GET() {
-    try {
-        const userId = headers().get("x-user-id")!;
-
-        const boards = await appwriteSDK.databases.listDocuments(
-            DB.id,
-            DB.collections.BOARDS_ID
-            // [
-            //     Query.equal("userId", userId)
-            // ]
-        );
-
-        return NextResponse.json(boards);
-    } catch (error) {
-        return NextResponse.json({ error });
-    }
-}
+// export async function GET() {
+//     try {
+//         const userId = headers().get("x-user-id")!;
+//
+//         const boards = await appwriteSDK.databases.listDocuments(
+//             DB.id,
+//             DB.collections.BOARDS_ID
+//             // [
+//             //     Query.equal("userId", userId)
+//             // ]
+//         );
+//
+//         return NextResponse.json(boards);
+//     } catch (error) {
+//         return NextResponse.json({ error });
+//     }
+// }
 
 
 export function PUT() {
